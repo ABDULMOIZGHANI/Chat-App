644fab3cab240086533c3b88d93f13a31a37770e
ABDUL MOIZ <babamoiz289@gmail.com>
2025-10-15 02:15:27 +0500

    front end auth slice

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,33 @@
 import "./App.css";
 import "react-toastify/dist/ReactToastify.css";
 import { Loader } from "lucide-react";
+import { useDispatch, useSelector } from "react-redux"
+import { useEffect } from "react";
+import { getUser, serOnlineUsers } from "./store/slices/authSlice";
+import { connectSocket, disconnectSocket } from "./lib/socket";
+
 const App = () => {
+  const { authUser, isCheckingAuth } = useSelector(state => state.auth)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(getUser())
+  }, [getUser])
+
+  useEffect(() => {
+    if (authUser) {
+      const socket = connectSocket(authUser._id)
+
+      socket.on("getOnlineUsers", (users) => {
+        dispatch(serOnlineUsers(users))
+      })
+
+      return () => disconnectSocket()
+    }
+  }, [authUser])
+
+
+
   return <></>;
 };
 
