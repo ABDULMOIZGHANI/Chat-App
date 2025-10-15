a41914817e500f315d7385ff027c7298e06c64e1
ABDUL MOIZ <babamoiz289@gmail.com>
2025-10-16 01:12:26 +0500

    App .jsx file code completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,8 @@ import { useDispatch, useSelector } from "react-redux"
 import { useEffect } from "react";
 import { getUser, serOnlineUsers } from "./store/slices/authSlice";
 import { connectSocket, disconnectSocket } from "./lib/socket";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom"
+import Navbar from "./components/Navbar.jsx"
 
 const App = () => {
   const { authUser, isCheckingAuth } = useSelector(state => state.auth)
@@ -26,9 +28,24 @@ const App = () => {
     }
   }, [authUser])
 
+  if (isCheckingAuth && !authUser) {
+    retuen(
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="size-10 animate-spins" />
+      </div>
+    )
+  }
 
-
-  return <></>;
+  return <>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={authUser ? <Home /> : <Navigate to={"/login"} />} />
+        <Route path="/register" element={!authUser ? <Register /> : <Navigate to={"/"} />} />
+        <Route path="/login" element={!authUser ? <Login /> : <Navigate to={"/"} />} />
+        <Route path="/profile" element={authUser ? <Profile /> : <Navigate to={"/login"} />} />
+      </Routes>
+    </Router></>;
 };
 
 export default App;
