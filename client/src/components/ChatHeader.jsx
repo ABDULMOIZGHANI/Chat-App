c39aadb5accd59eb291f1f792e70995670cf47c1
ABDUL MOIZ <babamoiz289@gmail.com>
2025-10-22 19:17:47 +0500

    design the chat header

--- client/src/components/ChatHeader.jsx
+++ client/src/components/ChatHeader.jsx
@@ -1,7 +1,47 @@
 import { X } from "lucide-react";
+import { useDispatch, useSelector } from "react-redux";
+import { setSelectedUser } from "../store/slices/chatSlice";
 
 const ChatHeader = () => {
-  return <></>;
+  const { selectedUser } = useSelector((state) => state.chat);
+  const { onLineUsers } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+
+  return (
+    <div className="p-3 border-b bg-gray-200 ring-1 ring-gray-300">
+      <div className="flex items-center justify-between">
+        {/* USER INFO */}
+        <div className="flex items-center gap-3">
+          {/* AVATAR */}
+          <div className="relative w-10 h-10">
+            <img
+              src={selectedUser?.avatar?.url || "/avatar-holder.avif"}
+              alt="avatar"
+              className="w-full h-full object-cover rounded-full"
+            />
+            {onLineUsers.includes(selectedUser?._id) && (
+              <span className="absolute bottom-0 right-0 w-3 h-3 bg-green-500 border-white border-2 rounded-full" />
+            )}
+          </div>
+
+          {/* USER NAME */}
+          <div>
+            <h3 className="font-semibold text-gray-900">
+              {selectedUser?.fullName}
+            </h3>
+            <p className="text-sm text-gray-600">
+              {onLineUsers.includes(selectedUser?._id) ? "Online" : "Offline"}
+            </p>
+          </div>
+        </div>
+
+        {/* CLOSE BUTTON */}
+        <button onClick={() => dispatch(setSelectedUser(null))} className="p-2 rounded-full hover:bg-gray-300 transition-colors">
+          <X size={20} />
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default ChatHeader;
